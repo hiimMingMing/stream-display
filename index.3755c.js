c799c10a9f44e9a6e5ceb314c3210c2496eca603
Hua Zan Ming <phamquangminhex@gmail.com>
2023-10-14 19:48:42 +0700

    - Update build

--- index.3755c.js
+++ index.3755c.js
@@ -1,4 +1,4 @@
-System.register(["./application.581a3.js"], function (_export, _context) {
+System.register(["./application.79697.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.581a3.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application581a3Js) {
-      createApplication = _application581a3Js.createApplication;
+    setters: [function (_application79697Js) {
+      createApplication = _application79697Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
