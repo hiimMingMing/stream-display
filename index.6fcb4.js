0c611ae3705b50931b300b3527e93233e5fc8de5
Hua Zan Mingz <phamquangminhex@gmail.com>
2023-10-25 15:59:52 +0700

    - Update build

--- index.6fcb4.js
+++ index.6fcb4.js
@@ -1,4 +1,4 @@
-System.register(["./application.23222.js"], function (_export, _context) {
+System.register(["./application.754b9.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.23222.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application23222Js) {
-      createApplication = _application23222Js.createApplication;
+    setters: [function (_application754b9Js) {
+      createApplication = _application754b9Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
